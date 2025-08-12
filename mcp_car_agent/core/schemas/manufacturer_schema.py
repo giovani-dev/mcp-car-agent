279ce0ed7cde06e2610054713964bdc9d20eb574
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2025-08-12 16:10:38 -0300

    fix: add id

--- mcp_car_agent/core/schemas/manufacturer_schema.py
+++ mcp_car_agent/core/schemas/manufacturer_schema.py
@@ -1,5 +1,7 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 
 
 class Manufacturer(BaseModel):
+    id: Optional[int] = None
     name: str = Field(max_length=150, min_length=1)
--- mcp_car_agent/core/schemas/transmission_schema.py
+++ mcp_car_agent/core/schemas/transmission_schema.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field
 
 
 class Transmission(BaseModel):
+    id: Optional[int] = None
     gearbox_type: str = Field(max_length=20, min_length=1)
     gears_qtde: Optional[int] = Field(default=None)
     traction: Optional[str] = Field(max_length=45, default=None)

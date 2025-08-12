f5c48e935ec625d7cb2aec06ce71faa48e69c1a1
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2025-08-12 15:38:13 -0300

    feat: cunit test config

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -2,18 +2,51 @@ from unittest.mock import AsyncMock
 
 import pytest
 
-from mcp_car_agent.core.database.repository.base_repository import BaseRepository
-from tests.mocks.models import MockModel
-from tests.mocks.schemas import MockSchema
+from mcp_car_agent.core.database.repository.car_repository import (
+    CarRepository,
+    CarSpecsRepository,
+)
+from mcp_car_agent.core.database.repository.engine_repository import (
+    EngineRepository,
+    EngineSpecRepository,
+)
 
 
-@pytest.fixture
-def mock_session():
+@pytest.fixture(name="mock_session")
+def setup_mock_session():
     """Cria uma sessão de mock assíncrona."""
     return AsyncMock()
 
 
 @pytest.fixture
-def repository(mock_session):
-    """Cria uma instância do BaseRepository com mocks."""
-    return BaseRepository(mock_session, model=MockModel, schema=MockSchema)
+def car_repository(mock_session):
+    """Cria uma instância de CarRepository com mocks."""
+    return CarRepository(session=mock_session)
+
+
+@pytest.fixture
+def car_spec_repository(mock_session):
+    """Cria uma instância de CarSpecsRepository com mocks."""
+    return CarSpecsRepository(session=mock_session)
+
+
+@pytest.fixture
+def engine_repository(mock_session):
+    """Cria uma instância de EngineRepository com mocks."""
+    return EngineRepository(session=mock_session)
+
+
+@pytest.fixture
+def engine_spec_repository(mock_session):
+    """Cria uma instância de EngineSpecRepository com mocks."""
+    return EngineSpecRepository(session=mock_session)
+
+
+@pytest.fixture
+def mock_config(monkeypatch):
+    """Mocks as variáveis de ambiente de configuração."""
+    monkeypatch.setattr("mcp_car_agent.core.config.DB_USER", "user")
+    monkeypatch.setattr("mcp_car_agent.core.config.DB_PWD", "pwd")
+    monkeypatch.setattr("mcp_car_agent.core.config.DB_HOST", "host")
+    monkeypatch.setattr("mcp_car_agent.core.config.DB_PORT", "port")
+    monkeypatch.setattr("mcp_car_agent.core.config.DB_NAME", "db")
